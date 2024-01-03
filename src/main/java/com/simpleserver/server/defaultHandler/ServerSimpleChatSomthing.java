74429f9a358c84c78d97fbf63507b79ce1a60289
Anton <a.tashchilin@bk.ru>
2024-01-03 17:03:19 +0400

    add new handler for chat

---
+++ src/main/java/com/simpleserver/server/defaultHandler/ServerSimpleChatSomthing.java
@@ -0,0 +1,38 @@
+package com.simpleserver.server.defaultHandler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import lombok.SneakyThrows;
+
+public class ServerSimpleChatSomthing implements Runnable {
+
+    private Socket socket;
+    
+
+    public ServerSimpleChatSomthing(Socket socket) {
+        this.socket = socket;
+    }
+
+
+    @Override
+    @SneakyThrows
+    public void run() {
+        try (var out = new PrintWriter(socket.getOutputStream(),true)) {
+            try (var in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                if (".".equals(inputLine)) {
+                    out.println("bye");
+                    break;
+                }
+                out.println(inputLine);
+            }
+        } 
+        socket.close();
+        }
+    }
+    
+}
