811d31c11f9ee70012ac9308dbf361100696148c
Anton <a.tashchilin@bk.ru>
2024-01-03 20:50:29 +0400

    change chat handler impl interface

--- src/main/java/com/simpleserver/server/defaultHandler/ServerSimpleChatHandler.java
+++ src/main/java/com/simpleserver/server/defaultHandler/ServerSimpleChatHandler.java
@@ -1,38 +1,66 @@
 package com.simpleserver.server.defaultHandler;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 import lombok.SneakyThrows;
 
-public class ServerSimpleChatHandler implements Runnable {
+public class ServerSimpleChatHandler implements ServerHandler {
 
     private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
     
 
-    public ServerSimpleChatHandler(Socket socket) {
-        this.socket = socket;
+
+    @Override
+    public void run() {
+        
+        String inputLine;
+        if(socket == null)
+            throw new RuntimeException("Not bind socket");
+        try {
+            while ((inputLine = in.readLine()) != null) {
+                if (".".equals(inputLine)) {
+                    out.println("bye");
+                    break;
+                }
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            shutdown();
+        }
+            
     }
+
+
+
 
 
     @Override
     @SneakyThrows
-    public void run() {
-        try (var out = new PrintWriter(socket.getOutputStream(),true)) {
-            try (var in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    if (".".equals(inputLine)) {
-                        out.println("bye");
-                        break;
-                    }
-                    out.println(inputLine);
-                }
-            } 
-            socket.close();
-        }
+    public void bindSocket(Socket socket) {
+        this.socket = socket;
+        this.out = new PrintWriter(socket.getOutputStream(),true);
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+
+
+
+
+    @Override
+    @SneakyThrows
+    public void shutdown() {
+        out.close();
+        in.close();
+        socket.close();
     }
     
 }
